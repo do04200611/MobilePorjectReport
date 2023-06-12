a5a20682a702bb2927a55f9b34927eacff075c36
kimganghyeon <kimganghyeon6080@naver.com>
2023-06-13 02:25:33 +0900

    Update MovieMap.java

--- java/MovieMap.java
+++ java/MovieMap.java
@@ -33,7 +33,7 @@ public class MovieMap extends AppCompatActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap map) {
         gMap = map;
         gMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.568256, 126.897240), 15));
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(35.1634, 129.1638), 15));
         gMap.getUiSettings().setZoomControlsEnabled(true);
         gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
@@ -52,10 +52,12 @@ public class MovieMap extends AppCompatActivity implements OnMapReadyCallback {
         super.onCreateOptionsMenu(menu);
         menu.add(0, 1, 0, "위성 지도");
         menu.add(0, 2, 0, "일반 지도");
-        SubMenu sMenu = menu.addSubMenu("유명장소 바로가기 >>");
-        sMenu.add(0, 3, 0, "정동진");
-        sMenu.add(0, 4, 0, "해운대");
-        sMenu.add(0, 5, 0, "땅끝마을");
+        SubMenu sMenu = menu.addSubMenu("영화관 바로가기 >>");
+        sMenu.add(0, 3, 0, "영화의 전당");
+        sMenu.add(0, 4, 0, "장산역 근처 영화관");
+        sMenu.add(0, 5, 0, "구서동 근처 영화관");
+        sMenu.add(0, 6, 0, "센텀시티 영화관");
+        sMenu.add(0, 7, 0, "해운대 영화관");
         return true;
     }
 
@@ -70,15 +72,23 @@ public class MovieMap extends AppCompatActivity implements OnMapReadyCallback {
                 return true;
             case 3:
                 gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
-                        37.689254, 129.033051), 15));
+                        35.158680, 129.164482), 15));
                 return true;
             case 4:
                 gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
-                        35.159003, 129.161882), 15));
+                        35.227714, 129.157886), 15));
                 return true;
             case 5:
                 gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
-                        34.301472, 126.524048), 15));
+                        35.184106, 129.083682), 15));
+                return true;
+            case 6:
+                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
+                        35.168815, 129.130721), 15));
+                return true;
+            case 7:
+                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
+                        35.161201, 129.161148), 15));
                 return true;
         }
         return false;
